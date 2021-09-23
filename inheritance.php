fdf50b0757f3e2c959b9e69b9c6e438a73bfc963
ajins15 <ansengarie@gmail.com>
2021-09-23 11:34:02 +0700

    menerapkan konsep inheritance pada kasus sebelumnya

---
+++ inheritance.php
@@ -0,0 +1,81 @@
+<?php
+
+class Produk
+{
+  public $judul,
+    $penulis,
+    $penerbit,
+    $harga,
+    $jmlHalaman,
+    $waktuMain,
+    $tipe;
+
+  public function __construct(
+    $judul = "Judul",
+    $penulis = "Penulis",
+    $penerbit = "Penerbit",
+    $harga = 0,
+    $jmlHalaman = 0,
+    $waktuMain = 0,
+    $tipe = "Tipe"
+  ) {
+    $this->judul = $judul;
+    $this->penulis = $penulis;
+    $this->penerbit = $penerbit;
+    $this->harga = $harga;
+    $this->jmlHalaman = $jmlHalaman;
+    $this->waktuMain = $waktuMain;
+    $this->tipe = $tipe;
+  }
+
+  public function getLabel()
+  {
+    return "$this->penulis, $this->penerbit";
+  }
+
+  public function getInfoProduk()
+  {
+    $str = "{$this->tipe}: {$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+    if ($this->tipe == "Komik") {
+      $str .= " - {$this->jmlHalaman} Halaman. ";
+    }
+    if ($this->tipe == "Game") {
+      $str .= " ~ {$this->waktuMain} Jam.";
+    }
+    return $str;
+  }
+}
+
+class Komik extends Produk
+{
+  public function getInfoProduk()
+  {
+    $str = "Komik: {$this->judul} | {$this->getLabel()} (Rp.{$this->harga}) - {$this->jmlHalaman} Halaman.";
+    return $str;
+  }
+}
+class Game extends Produk
+{
+  public function getInfoProduk()
+  {
+    $str = "Game: {$this->judul} | {$this->getLabel()} (Rp.{$this->harga}) ~ {$this->waktuMain} Jam.";
+    return $str;
+  }
+}
+
+class CetakInfoProduk
+{
+  public function cetak(Produk $produk)
+  {
+    $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+    return $str;
+  }
+}
+
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0, "Komik");
+
+$produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000, 0, 50, "Game");
+
+echo $produk1->getInfoProduk();
+echo '<br>';
+echo $produk2->getInfoProduk();
